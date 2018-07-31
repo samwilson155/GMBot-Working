f43033c6910c350cb991af2eb58c7717a6e96d7b
samwilson155 <40063691+samwilson155@users.noreply.github.com>
2018-07-31 14:04:18 -0400

    Final

--- bot.js
+++ bot.js
@@ -12,6 +12,8 @@ function respond() {
 	  botRegex4 = /\bPA Minerva\b/;
 	  botRegex5 = /\blol\b/;
 	  botRegex6 = /\bLol\b/;
+	  botRegex7 = /\bthe\b/;
+	  botRegex8 = /\bThe\b/;
 	  
 
   if(request.text && botRegex.test(request.text)) {
@@ -67,6 +69,26 @@ function respond() {
     if(request.text && botRegex6.test(request.text)) {
     this.res.writeHead(200);
     postMessage3();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+
+    if(request.text && botRegex7.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage4();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+
+    if(request.text && botRegex8.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage4();
     this.res.end();
   } else {
     console.log("don't care");
@@ -149,6 +171,41 @@ function postMessage3() {
   var botResponse, options, body, botReq;
 
   botResponse = 'lmao what yall laughin at?';
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postMessage4() {
+  var botResponse, options, body, botReq;
+
+  botResponse = cool();
 
   options = {
     hostname: 'api.groupme.com',
