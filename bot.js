13c4c8d38d0a10ac65b416e7a7abd1ac057b1f7c
samwilson155 <40063691+samwilson155@users.noreply.github.com>
2018-08-01 09:29:44 -0400

    ggg

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ function respond() {
 	  botRegex10 = /\whaha\w/;
 	  botRegex7 = /\bthe\b/;
 	  botRegex8 = /\bThe\b/;
-	  botRegex9 = /\bWeather\b/;
+	  botRegex9 = /\@Minerva\b/;
 	  
 
   if(request.text && botRegex.test(request.text)) {
@@ -226,6 +226,41 @@ function postMessage4() {
   var botResponse, options, body, botReq;
 
   botResponse = cool();
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postMessage5() {
+  var botResponse, options, body, botReq;
+
+  botResponse = `Don't mention me hoe. Ill scrape you`;
 
   options = {
     hostname: 'api.groupme.com',
